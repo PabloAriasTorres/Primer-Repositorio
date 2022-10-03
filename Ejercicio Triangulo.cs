c242024d15f2c42c178d8d49be14574d7c878702
Pablo Arias <Pablokid08@gmail.com>
2022-10-03 15:22:22 +0200

    Con el trycatch

--- Ejercicio Triangulo.cs
+++ Ejercicio Triangulo.cs
@@ -4,24 +4,31 @@ class Ejercicio_Triángulo
 	static void Main()
 	{
 		int tamaño;
-		Console.WriteLine("Introduce el tamaño del triángulo:");
-		tamaño=Convert.ToInt32(Console.ReadLine());
-		
-		for(int i=1; i<=tamaño; i++)
+		try
 		{
-			for(int h=1; h<=tamaño-i; h++)
+			Console.WriteLine("Introduce el tamaño del triángulo:");
+			tamaño=Convert.ToInt32(Console.ReadLine());
+		
+			for(int i=1; i<=tamaño; i++)
 			{
-				Console.Write(" ");
+				for(int h=1; h<=tamaño-i; h++)
+				{
+					Console.Write(" ");
+				}
+				for(int j=1; j<=i; j++)
+				{
+					Console.Write("*");
+				}
+				for(int k=1; k<=i-1; k++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
 			}
-			for(int j=1; j<=i; j++)
-			{
-				Console.Write("*");
-			}
-			for(int k=1; k<=i-1; k++)
-			{
-				Console.Write("*");
-			}
-			Console.WriteLine();
+		}
+		catch(FormatException)
+		{
+			Console.WriteLine("No es número válido");
 		}
 	}
 }
